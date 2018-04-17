4cfe73df437bc6d36b9f09a6c7082e8a45c7b239
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-04-17 10:38:34 +0200

    ajax fixed to get 12 results and only from GB and US

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@
 /*  Employee Directory     */
 /*=========================*/
 
+let employees = null;
+
+  $.ajax({
+    url: 'https://randomuser.me/api/?nat=us,gb&results=12',
+    dataType: 'json',
+    success: function(data) {
+      employees = data.results;
+    }
+  });
+
