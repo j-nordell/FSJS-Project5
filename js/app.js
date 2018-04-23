022ffd1e1637165a14de7b449e890840c76b63d7
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-04-23 23:36:41 +0200

    basic styling added

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ function initApplication() {
 function fillCards() {
   for(let i = 0; i < employees.length; i++) {
     let currentCard = document.getElementById(`employee${i}`);
-    currentCard.getElementsByTagName("img")[0].setAttribute("src", `${employees[i].picture.medium}`);
+    currentCard.getElementsByTagName("img")[0].setAttribute("src", `${employees[i].picture.large}`);
     currentCard.getElementsByClassName("fullname")[0].textContent = `${employees[i].name.first}  ${employees[i].name.last}`;
     currentCard.getElementsByClassName("email")[0].textContent = `${employees[i].email}`;
     currentCard.getElementsByClassName("city")[0].textContent = `${employees[i].location.city}`;
