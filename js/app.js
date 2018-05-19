37286b15dda99651d384e0ae36a7908a320a24ed
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-05-19 09:37:05 +0200

    the modal doesn't work - x in wrong place

--- js/app.js
+++ js/app.js
@@ -22,8 +22,8 @@ function getEmployees() {
 }
 
 function initApplication() {
-  console.log(employees);
   fillCards();
+  setupModal();
 }
 
 function fillCards() {
@@ -34,4 +34,40 @@ function fillCards() {
     currentCard.getElementsByClassName("email")[0].textContent = `${employees[i].email}`;
     currentCard.getElementsByClassName("city")[0].textContent = `${employees[i].location.city}`;
   }
-}
+ }
+
+ function setupModal() {
+   // Get the modal
+    let modal = document.getElementById('modal');
+
+    //let card = document.getElementById("employee0");
+    let cards = document.getElementsByClassName("card");
+
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    for(let card of cards) {
+      card.onclick = () => {
+        modal.style.display = "block";
+        let index = card.id;
+        console.log(index);
+        fillModal();
+      };
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = () => {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = (e) => {
+        if (e.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    function fillModal(e) {
+      let modalContent = document.getElementById("modal-content");
+      modalContent.getElementsByTagName("img")[0].setAttribute("src", `${employees[0].picture.large}`);
+    }
+ }
